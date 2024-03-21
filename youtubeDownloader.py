689d92ca60c2f4535e55e44ba0446db016745e57
Noussse <149839925+Noussse@users.noreply.github.com>
2024-03-21 17:22:25 +0100

    Update youtubeDownloader.py

--- youtubeDownloader.py
+++ youtubeDownloader.py
@@ -1,7 +1,9 @@
 from pytube import YouTube
+import os
 url = input("YouTube video link: ")
 yt = YouTube(url)
 print("Downloading", yt.title)
+downloads_folder = os.path.join(os.path.expanduser('~'), 'Downloads')
 res = yt.streams.get_highest_resolution()
-res.download(r'C:\Users\nouss\Downloads')
-print("download comlete")
+res.download(downloads_folder)
+print("Download complete")
